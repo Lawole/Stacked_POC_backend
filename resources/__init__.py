b6a068513ce9ed2a8301169bbf3fcb888ade7689
Dosunmu Olawole <103733962+Lawole@users.noreply.github.com>
2024-02-17 13:35:55 +0100

    initial commit

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask, request
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://backend_p1kj_user:uoe8VqwRnlpYXkHaqne4gZdu3viSGTJI@dpg-cn7h9a2cn0vc738uud0g-a.oregon-postgres.render.com/backend_p1kj'
+app.config['SQLALCHEMY_DATABASE_URI'] = ('postgresql://backend_p1kj_user:uoe8VqwRnlpYXkHaqne4gZdu3viSGTJI@dpg-cn7h9a2cn0vc738uud0g-a.oregon-postgres.render.com/backend_p1kj')
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from resources.models import Customers
+
+from resources.models import Customers, Webhooks
 
 
 @app.route('/register', methods=['POST'])
@@ -36,12 +38,27 @@ def register_customer():
         'email': email,
         'password': password
     }, 200
-# db.init_app(app)
+
+
+# webhook URL endpoint
+@app.route('/api/webhook', methods=['Post'])
+def handle_webhook():
+    payload = request.get_json()
+    resources = payload.get('resources')
+    eventName = payload.get('eventName')
+    flowID = payload.get('flowID')
+    timestamp = payload.get('timestamp')
+
+    new_webhook = Webhooks(resources=resources,
+                           eventName=eventName,
+                           flowID=flowID,
+                           timestamp=timestamp)
+
+    db.session.add(new_webhook)
+    db.session.commit()
+
+    return {'message': 'Webhooks received'}, 201
 
 
 with app.app_context():
     db.create_all()
-
-
-
-
--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,5 @@
+from _datetime import datetime
+
 from resources import db
 
 
@@ -12,3 +14,14 @@ class Customers(db.Model):
     image_file = db.Column(db.String(120), nullable=True, default='default.jpg')
     password = db.Column(db.String(250), unique=False, nullable=True)
     document = db.Column(db.String(250), unique=False, nullable=True)
+
+
+class Webhooks(db.Model):
+    __tablename__ = 'webhooks'
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    resources = db.Column(db.String(1000), unique=False, nullable=True)
+    eventName = db.Column(db.String(250), unique=False, nullable=True)
+    flowID = db.Column(db.String(1000), unique=False, nullable=True)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime)
